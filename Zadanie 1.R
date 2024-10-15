9d086ceff7654c25b0c8e43436b38e5033944c46
Motrenos <morenko342@gmail.com>
2024-10-15 11:00:40 +0200

    Add files via upload

---
+++ Zadanie 1.R
@@ -0,0 +1,27 @@
+## Zadanie 1: Zapoznanie się z RStudio
+
+**Cel:** Poznanie interfejsu i podstawowych funkcji RStudio.
+
+### Instrukcje i rozwiązanie:
+
+1.  **Uruchom RStudio** na swoim komputerze.
+
+2.  **Zapoznaj się z interfejsem użytkownika:**
+  
+  3.  **Stwórz nowy skrypt R:**
+  
+  4.  **W skrypcie napisz komentarze** opisujące funkcje poszczególnych paneli.
+
+#Source (Edytor skryptów) służy do pisania skrypów, a funkcje nie są automatycznie wykonywane (Konsola) służy do egzekucji.
+
+5.  **Znajdź w RStudio ustawienia** i **zmień motyw kolorystyczny** na taki, który najbardziej Ci odpowiada.
+
+# Motyw kolorystyczny
+
+6.  **Napisz w komentarzu**, jaki motyw wybrałeś/aś i dlaczego.
+
+# Nie działa mi tools
+
+7.  **Zapisz skrypt** jako `zadanie1.R` w swoim folderze.
+
+8.  **Prześlij plik do swojego repozytorium na GitHub**.
